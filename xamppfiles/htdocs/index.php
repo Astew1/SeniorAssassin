a050eecbfcdca23b14873dbcfc91add5f9817714
Alexander Briasco-Stewart <astewart.is1@gmail.com>
2016-10-14 11:01:31 -0400

    symbolic links

--- xamppfiles/htdocs/index.php
+++ xamppfiles/htdocs/index.php
@@ -1,4 +1,8 @@
 <?php
 	require_once("init.php");
+  //echo $_SESSION['LoggedIn'];
+	//echo $_SESSION['Username'];
+
+	//WHY THE HECK DOES THIS BREAK IF THIS LINE IS IN?!?!??!
 	header("Location:index.html");
 ?>
