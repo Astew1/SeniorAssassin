fb5bba5422a1b2d7c186717578f821a268923918
Alexander Briasco-Stewart <astewart.is1@gmail.com>
2016-10-24 13:48:37 -0400

    fixing login?

--- xamppfiles/htdocs/init.php
+++ xamppfiles/htdocs/init.php
@@ -9,11 +9,11 @@ include_once("dbconnect.php");
 // mysql_connect($dbhost, $username, $password);
 // @mysql_select_db($database) or die( "Unable to select database");
 session_start();
-if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['username']))
+if(!empty($_SESSION['LoggedIn']) && !empty($_SESSION['Username']))
 {
     $loggedIn = 1;
 }
-elseif(!empty($_POST['username']) && !empty($_POST['password']))
+elseif(!empty($_POST['Username']) && !empty($_POST['password']))
 {
 
     header("./login.php");
@@ -23,9 +23,7 @@ else
 {
     header("Location:loginform.html");
 }
-echo $loggedIn;
-echo $_SESSION['LoggedIn'];
-echo $_SESSION['username'];
+
 
 
 
